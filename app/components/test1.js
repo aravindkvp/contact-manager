a792f8b63bdc9a098471b21c56a7f6b4e2e607b0
Aravind <71426922+aravindkvp@users.noreply.github.com>
2020-09-29 14:27:14 +0530

    Update test1.js

--- app/components/test1.js
+++ app/components/test1.js
@@ -3,5 +3,8 @@ import Component from '@ember/component';
 export default Component.extend({
     name: 'Aravind',
     department: 'CSE',
-    regNo: 1612015
+    regNo: 1612015.
+    call(){
+      console.log('call');
+    }
 });
