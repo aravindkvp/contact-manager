92c6ffb0717f5b2fbd2c4a99a0216382addd217d
Aravind <aravind.yc@zohocorp.com>
2020-09-30 17:53:03 +0530

    verison1

--- app/components/test1.js
+++ app/components/test1.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-    name: 'Al Sameema',
-    regNo: 1612007,
+    name: 'Aravind',
+    regNo: 1612015,
     department: 'CSE',
 });
